68a82247a02a45e8bcc2571fd576e2bed3cab6b4
Wakhid Wicaksono <wichaksono.w@gmail.com>
2025-07-30 02:32:41 +0700

    update

---
+++ inc/dev-tools/src/Modules/MenuHider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace NeonWebId\DevTools\Modules;
+
+use NeonWebId\DevTools\Contracts\BaseModule;
+
+final class MenuHider extends BaseModule
+{
+    public function id(): string
+    {
+        // TODO: Implement id() method.
+    }
+
+    public function title(): string
+    {
+        // TODO: Implement title() method.
+    }
+
+    public function name(): string
+    {
+        // TODO: Implement name() method.
+    }
+
+    public function content(): void
+    {
+        // TODO: Implement content() method.
+    }
+
+    public function apply(): void
+    {
+        // TODO: Implement apply() method.
+    }
+}
